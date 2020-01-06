97f268aa9c8c057251b7ca9d56ddc2b1a0f75e57
Sho Mizutani <lowply@github.com>
2020-01-06 19:58:53 +0900

    Move d.Dates down

--- src/date.go
+++ src/date.go
@@ -33,13 +33,13 @@ func NewDate(t time.Time) *date {
 	d.WeekStartDate = n.BeginningOfWeek().Format("2006/01/02")
 	_, isoweek := n.Monday().ISOWeek()
 	d.WeekNumber = fmt.Sprintf("%02d", isoweek)
-	for j := range d.Dates {
-		d.Dates[j] = n.Monday().AddDate(0, 0, j).Format("01/02")
-	}
 	// Thursday of the week, should be used with the week number
 	// e.g. "2020 Week 01".
 	// See https://en.wikipedia.org/wiki/ISO_week_date#First_week
 	// for the ISO 8601 first week definition
 	d.WeekNumberYear = n.BeginningOfWeek().AddDate(0, 0, 3).Format("2006")
+	for j := range d.Dates {
+		d.Dates[j] = n.Monday().AddDate(0, 0, j).Format("01/02")
+	}
 	return d
 }
